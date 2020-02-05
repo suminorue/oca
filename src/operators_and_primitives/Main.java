1547e31e0e49795b70b66eaf002eefea98a04fd8
Volodymyr Sazonov <eurohome1998@gmail.com>
2020-02-05 16:26:23 +0200

    added to test_branch

--- src/operators_and_primitives/Main.java
+++ src/operators_and_primitives/Main.java
@@ -7,18 +7,25 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        short x = 2;
-        short y = 2;
+//        short x = 2;
+//        short y = 2;
+//
+//        System.out.println(x*y); //this one is ok
+//
+//        //short z =x * y;    //this one will cause compile error(on arithmetic operation short is casted to int by default)
+//
+//        int x1;
+//        int y1 = (x1 = 3);
+//        System.out.println(x1 + ":" + y1);
+//
+//        Main main = new Main();
 
-        System.out.println(x*y); //this one is ok
-
-        //short z =x * y;    //this one will cause compile error(on arithmetic operation short is casted to int by default)
-
-        int x1;
-        int y1 = (x1 = 3);
-        System.out.println(x1 + ":" + y1);
-
-        Main main = new Main();
+        System.out.println(count7(712377));
+    }
+    public static int count7(int n) {
+        if (n == 0) return 0;
+        if (n%10 == 7) return 1+count7(n/10);
+        return 0+count7(n/10);
     }
     {
         System.out.println("static initializer"); //static initializer and fields are initialized before constructor called
